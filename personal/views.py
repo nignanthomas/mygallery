6e4f8e12c5bf1940291b4dc4abef5da2756b5f9b
nignanthomas <nignanthomas@gmail.com>
2019-02-28 15:50:39 +0300

    create landing view function

--- personal/views.py
+++ personal/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
+from .models import Location,Category,Image
+
 
 # Create your views here.
+
+
+def landing(request):
+    all_images = Image.objects.all()
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    title = 'Home'
+
+    return render(request,'index.html', {'all_images':all_images,'locations':locations,'categories':categories, 'title':title})
