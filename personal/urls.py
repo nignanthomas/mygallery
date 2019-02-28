ca8b4d1fa1322b7262dad34f51b01f5e0a9d0876
nignanthomas <nignanthomas@gmail.com>
2019-02-28 18:44:20 +0300

    register MEDIA_ROOT to personal.urls.py

--- personal/urls.py
+++ personal/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 
@@ -8,3 +9,5 @@ urlpatterns = [
     url(r'^search/',views.search_results, name='search_results'),
 
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
