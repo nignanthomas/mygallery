029a6227d0d8025fd2c0a58a868675e386fb020b
nignanthomas <nignanthomas@gmail.com>
2019-03-02 15:17:16 +0300

    update search_results view function

--- personal/views.py
+++ personal/views.py
@@ -23,7 +23,7 @@ def search_results(request):
     if 'searchcat' in request.GET and request.GET["searchcat"]:
         search_term = request.GET.get("searchcat")
         searched_images = Image.search_category(search_term)
-        message = f"{search_term}"
+        message = f"Results for: {search_term}"
 
         return render(request, 'index.html',{"message":message,"all_images": searched_images,'locations':locations,'categories':categories, 'title':title})
 
